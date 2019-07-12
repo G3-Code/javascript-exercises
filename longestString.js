148beaa4009f819a395b99ac6cb18c37de2db848
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-11 22:21:04 -0500

    Longest-String: First Try

--- longestString.js
+++ longestString.js
@@ -1,6 +1,23 @@
 function getLongest(sen) {
-  // code here
-  return sen;
+  const words = sen.split(" ");
+  const wordLengths = words.map(word => {
+    return { word: word, length: word.length };
+  });
+  wordLengths.sort(compare);
+  return wordLengths[0].word;
+}
+
+function compare(a, b) {
+  const length1 = a.length;
+  const length2 = b.length;
+
+  let comparison = 0;
+  if (length1 > length2) {
+    comparison = -1;
+  } else {
+    comparison = 1;
+  }
+  return comparison;
 }
 
 console.log(getLongest("hello world")); // hello
