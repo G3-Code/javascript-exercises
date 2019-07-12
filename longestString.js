3307b13c9a20b0bd0bb97bd4150aa432797b977a
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-07-11 22:32:22 -0500

    Longest String: Third Try - updated to check for special characters

--- longestString.js
+++ longestString.js
@@ -1,7 +1,18 @@
-function getLongest(sentence) {
-  const words = sentence.split(" ");
-  const longestWord = words.reduce(function(a, b) {
-    return a.length > b.length ? a : b;
+function getLongest(sen) {
+  const words = sen.split(" ");
+  const refinedWords = words.map(word => {
+    return word.replace(/[&\/\\#,+()$~%.'"@:*?<>{}]/g, "");
+  });
+  console.log(refinedWords);
+  const longestWord = refinedWords.reduce(function(a, b) {
+    if (a.length > b.length) {
+      return a;
+    } else if (a.length === b.length) {
+      return a;
+    } else {
+      return b;
+    }
+    //return a.length > b.length ? a : b;
   });
   return longestWord;
 }
